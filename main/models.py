da41c78f560e13af4b945fb658930fcc3f0e507c
Michael Goff <msgoff@users.noreply.github.com>
2019-12-03 09:43:12 -0800

    Update models.py

--- main/models.py
+++ main/models.py
@@ -1,20 +1,22 @@
 from django.db import models
 
 
-# Create your models here.
 class Stories(models.Model):
     id = models.IntegerField(primary_key=True)
     article = models.CharField(max_length=200, blank=True)
     author = models.CharField(max_length=350, blank=True)
     comments = models.CharField(max_length=200, blank=True)
     domain = models.CharField(max_length=350, blank=True)
-    points = models.IntegerField(blank=True)
+    points = models.IntegerField(blank=True, null=True)
     time_stamp = models.CharField(max_length=200, blank=True)
     by = models.CharField(max_length=256, blank=True, null=True)
-    descendants = models.CharField(max_length=256, blank=True, null=True)
+    descendants = models.IntegerField(max_length=256, blank=True, null=True)
     kids = models.CharField(max_length=256, blank=True, null=True)
-    score = models.CharField(max_length=256, blank=True, null=True)
-    time = models.CharField(max_length=256, blank=True, null=True)
+    score = models.IntegerField(max_length=256, blank=True, null=True)
+    time = models.IntegerField(max_length=256, blank=True, null=True)
     title = models.CharField(max_length=256, blank=True, null=True)
     type = models.CharField(max_length=256, blank=True, null=True)
     url = models.CharField(max_length=256, blank=True, null=True)
+    dead = models.BooleanField(blank=True, null=True)
+    text = models.TextField(blank=True, null=True)
+
