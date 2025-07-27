93eb7a0b564e00f6dabf4ef873fbfd6b580cd94b
mike <mike@mathrules.us>
2025-07-27 02:04:58 +0000

    off_by_one

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-for i in `seq 2001 999 10000`;do screen -dmS $i python get_stories.py $i $(($i + 999));done
+for i in `seq 2001 999 10000`;do screen -dmS $i python get_stories.py $(($i+1)) $(($i + 999));done
 
