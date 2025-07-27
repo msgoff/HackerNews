ec0bdc351dbdfe02ed2492e408113fd0afa2ef35
mike <mike@mathrules.us>
2025-07-27 02:32:54 +0000

    test_run

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-for i in `seq 20000 999 30000`;do screen -dmS $i python get_stories.py $(($i)) $(($i + 1000));done
+for i in `seq 30000 1000 40000`;do  screen -dmS $i python get_stories.sh  $(($i+1)) $(($i + 1000));done
